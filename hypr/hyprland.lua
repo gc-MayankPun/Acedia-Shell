3c8f33a2cf68f5b97d9971cc8ba510a26a0f59f1
gc-MayankPun <punmayank35@gmail.com>
2026-08-28 01:19:53 +0530

    Add keybinds for screenshots

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -36,7 +36,7 @@ hl.monitor({
 
 -- Set programs that you use
 local terminal    = "kitty"
-local fileManager = "dolphin"
+local fileManager = "nemo"
 local menu        = "hyprlauncher"
 
 
@@ -311,6 +311,13 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
+-- Misc keybinds
+
+-- Screenshot entire screen
+hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("grim ~/Pictures/Screenshots/$(date +'%Y-%m-%d_%H-%M-%S').png"))
+
+-- Screenshot selected area
+hl.bind(mainMod .. " + SHIFT + PRINT", hl.dsp.exec_cmd("grim -g \"$(slurp)\" ~/Pictures/Screenshots/$(date +'%Y-%m-%d_%H-%M-%S').png"))
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
